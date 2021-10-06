9f61b6a9654b9476ad86743789e14aa78605e6f1
Roman Kotelnikov <rproger@mail.ru>
2021-10-07 03:58:09 +0800

    переделал функции на стрелочные, исправил ошибку с заголовками

--- js/script.js
+++ js/script.js
@@ -7,26 +7,44 @@ let text = [
     "вас потопил осьминог"
 ];
 
-let delete_gif = function() {
-    $("#gifka").remove();
-    $("#title_alert").remove();
-}
+let attempts = 0;
 
-let coordinats = function(name) {
+//удаляет гифку с текстом
+let delete_gif = () => {
+    $("#gifka").detach();
+    $("#title_alert").detach();
+};
+
+//движение корабля
+let move_the_ship = obj => {
+    $("html").click(function(event) {
+        $(obj).offset({
+            top: event.pageY,
+            left: event.pageX
+        });
+    });
+};
+
+/*
+name - id блока при клике на который
+срабатывает определённое условие
+*/
+let id_name = name => {
     if(name == "peski") {
-        $("body").append('<img id="gifka" src=' +gifs[0]+ ' alt="peski">');
-        $("body").append('<h2 id="title_alert">' +text[0]+ '</h1>');
+        $("body").append('<img id="gifka" src="' +gifs[0]+ '" alt="peski">');
+        $("body").append('<h2 id="title_alert">' +text[0]+ '</h2>');
     }
     if(name == "osminog") {
-        $("body").append('<img id="gifka" src=' +gifs[1]+ ' alt="peski">');
-        $("body").append('<h2 id="title_alert">' +text[1]+ '</h1>');
+        $("body").append('<img id="gifka" src="' +gifs[1]+ '" alt="peski">');
+        $("body").append('<h2 id="title_alert">' +text[1]+ '</h2>');
     }
-}
+};
 
-let click_map = function(event) {
-    coordinats(event.target.id);
-    setTimeout(delete_gif, 5000);
-    console.log(event.pageX+ " " +event.pageY+ " element: " +event.target.id);
-}
+let my_time = 5000;
+let click_map = event => {
+    move_the_ship("#corabl");
+    id_name(event.target.id);
+    setTimeout(delete_gif, my_time);
+};
 
 $("body").click(click_map);
